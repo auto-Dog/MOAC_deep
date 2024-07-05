87a246d7af6577fff377bc6f041b77265a20f048
auto-Dog <2969860272@qq.com>
2024-07-05 14:34:10 +0800

    Sensor dataset

--- src/gen_dataset.py
+++ src/gen_dataset.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader
 from PIL import Image
 from scipy import signal
 from tqdm import tqdm
+import pandas
 
 # 定义文件夹路径
 train_gt_path = '../dataset/train/GT'
@@ -21,64 +22,65 @@ os.makedirs(train_noised_path, exist_ok=True)
 os.makedirs(test_gt_path, exist_ok=True)
 os.makedirs(test_noised_path, exist_ok=True)
 
-# 定义灰度转换和上采样的transform
-transform = transforms.Compose([
-    transforms.Grayscale(),
-    transforms.ToTensor(),
-    transforms.Resize((64, 64),antialias=False),    # 注意：必须先转成tensor再进行无锯齿缩放。否则图像会因为抗锯齿插值算法变模糊
-])
+# # 定义灰度转换和上采样的transform
+# transform = transforms.Compose([
+#     transforms.Grayscale(),
+#     transforms.ToTensor(),
+#     transforms.Resize((64, 64),antialias=False),    # 注意：必须先转成tensor再进行无锯齿缩放。否则图像会因为抗锯齿插值算法变模糊
+# ])
 
-# 加载CIFAR-10数据集
-train_dataset = CIFAR10(root='../dataset/data', train=True, download=True, transform=transform)
-test_dataset = CIFAR10(root='../dataset/data', train=False, download=True, transform=transform)
+# # 加载CIFAR-10数据集
+# train_dataset = CIFAR10(root='../dataset/data', train=True, download=True, transform=transform)
+# test_dataset = CIFAR10(root='../dataset/data', train=False, download=True, transform=transform)
 
-train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
+# train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+# test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 
-def generate_gt_and_noised_images(loader, gt_path, noised_path):
+train_files = ['20160930_203718.csv','20161001_231809.csv','20161007_210049.csv','20161011_113032.csv','20161006_182224.csv']
+test_files = ['20161014_184659.csv','20161016_053656.csv']
+
+def generate_gt_and_noised_images(file_list, gt_path, noised_path):
     # 处理数据集中的每一张图像
-    slices = []
-    for i, (image, _) in tqdm(enumerate(loader)):
-        image = image.squeeze(0).squeeze(0)  # 去掉批次和通道维度
-        # print(image.size()) # debug
-        # image_s = transforms.ToPILImage()(image) # debug
-        # image_s.save(os.path.join(gt_path, f'check{i}.png')) # debug
-        # 将图像裁剪成20x64的切片
-        for j in range(4):
-            x_start = random.randint(0, 44)  # 0到44之间随机裁剪起始位置
-            slice = image[x_start:x_start+20,:]
-            slices.append(slice)
-
-        # 拼接切片，来自多张图片
-        if (i + 1) % 5 == 0:
-            file_name = i//5
-            combined_slices = torch.cat(slices, dim=1)
-            while combined_slices.size(1) < 1000:
-                combined_slices = torch.cat((combined_slices, combined_slices), dim=1)
-            combined_slices = combined_slices[:, :1000]
+    root = '../dataset/archive/'
+    for file_i,file in enumerate(file_list):
+        file = os.path.join(root,file)
+        df = pandas.read_csv(file,encoding='UTF-8') 
+        sensor_df = df[['R1 (MOhm)','R2 (MOhm)','R3 (MOhm)','R4 (MOhm)','R5 (MOhm)',
+                       'R6 (MOhm)','R7 (MOhm)','R8 (MOhm)','R9 (MOhm)','R10 (MOhm)',
+                       'R11 (MOhm)','R12 (MOhm)','R13 (MOhm)','R14 (MOhm)']]
+        sensor_array = sensor_df.to_numpy()
+        # print(sensor_array[:2,:])  # debug
+        total_len = sensor_array.shape[0]
+        for i in range(total_len//256-1):
+            start_index = i*256+np.random.randint(0,20) # 随机选择起点
+            data_slice = sensor_array[start_index:start_index+256,:]
+            data_slice = data_slice.T   # 14x256
+            file_name = file_i*1000+i
             # print(combined_slices.size())   # debug
             # 随机打乱4-6行
             random.seed(file_name)
             num_rows_to_shuffle = random.randint(4, 6)
-            rows_to_shuffle = random.sample(range(20), num_rows_to_shuffle)
-            shuffled_slices = combined_slices.clone()
+            rows_to_shuffle = random.sample(range(14), num_rows_to_shuffle)
+            shuffled_slices = data_slice.copy()
             for row in rows_to_shuffle:
-                random_row = random.choice(list(set(range(20)) - {row}))
-                shuffled_slices[row, :] = combined_slices[random_row, :]
-
+                random_row = random.choice(list(set(range(14)) - {row}))
+                shuffled_slices[row, :] = data_slice[random_row, :]
+            # print(shuffled_slices.shape)    # debug
             # 转换为PIL图像并保存
-            gt_image = transforms.ToPILImage()(shuffled_slices)
+            shuffled_slices[shuffled_slices>80.0] = 80.0
+            shuffled_slices_norm = (shuffled_slices-np.min(shuffled_slices))/(80.-np.min(shuffled_slices)) # 归一化
+            shuffled_slices_norm = np.uint8(shuffled_slices_norm * 255)
+            gt_image = Image.fromarray(shuffled_slices_norm)
             gt_image.save(os.path.join(gt_path, f'{file_name:05d}.png'))
 
             # 生成噪声图像, 训练时，根据seed直接可以得出SNR，h_coff
             random.seed(file_name+1)
             SNR_db = random.randint(-4, 4)*5    # -20dB ~ 20dB
             np.random.seed(file_name+2)
-            h_coff = np.exp(1j*np.random.uniform(0,1,(20,1)) * 4* np.pi /4) # e^j0 ~ e^j pi
+            h_coff = np.exp(1j*np.random.uniform(0,1,(14,1)) * 4* np.pi /4) # e^j0 ~ e^j pi
             noised_image = distortion_func(shuffled_slices,h_coff,SNR_db)
-            noised_image = transforms.ToPILImage()(noised_image)
+            noised_image = Image.fromarray(noised_image)
             noised_image.save(os.path.join(noised_path, f'{file_name:05d}_snr{SNR_db}.png'))
-            slices = []  # 清空图片集合
 
 def awgn(x, snr):
     '''Add AWGN(complex)
@@ -93,12 +95,12 @@ def awgn(x, snr):
     noise = noise_r + 1j*noise_i
     return x + noise
 
-def distortion_func(image:torch.Tensor,h_coff,SNR_db=0):
+def distortion_func(image:np.ndarray,h_coff,SNR_db=0):
     # 添加混叠，以及信道系数
     np.random.seed()   # 保证噪声随机化
-    image = image.numpy()
-    image = (image-np.min(image))/(np.max(image)-np.min(image)) # 归一化
-    M_users = 20
+    # image = image.numpy()
+    image = (image-np.min(image))/(80.-np.min(image)) # 归一化
+    M_users = 14
     blur_kernel = np.zeros((2*M_users-1,3))
     kernel_height = 2*M_users-1
     blur_kernel[0:M_users,1] = 1.
@@ -122,11 +124,12 @@ def distortion_func(image:torch.Tensor,h_coff,SNR_db=0):
     convSame = np.array([convSame.real,convSame.imag,np.zeros_like(convSame.real)])# 为保证计算效率，此处分离复数实部虚部
     convSame = (convSame-np.min(convSame))/(np.max(convSame)-np.min(convSame)) # 归一化
     convSame = np.uint8(convSame * 255)
-    convSame = torch.from_numpy(convSame)   # 注意：在显卡上运行时，此处加cuda()
+    convSame = np.transpose(convSame, (1, 2, 0)) # HxWxC
+    # convSame = torch.from_numpy(convSame)   # 注意：在显卡上运行时，此处加cuda()
     return convSame
 
 # 生成训练集GT和噪声图像
-generate_gt_and_noised_images(train_loader, train_gt_path, train_noised_path)
+generate_gt_and_noised_images(train_files, train_gt_path, train_noised_path)
 
 # 生成测试集GT和噪声图像
-generate_gt_and_noised_images(test_loader, test_gt_path, test_noised_path)
+generate_gt_and_noised_images(test_files, test_gt_path, test_noised_path)
--- src/restore_module.py
+++ src/restore_module.py
@@ -101,6 +101,7 @@ class TinyUNet(nn.Module):
         x3 = self.down2(x2)
         x = self.up1(x3, x2)
         x = self.up2(x, x1)
+        # xc = x1 + x     # residual path
         logits = self.outc(x)
         return logits
 
