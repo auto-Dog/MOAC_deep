5220579ec093ddd8cc0b9d9dc90e961301d35888
auto-Dog <2969860272@qq.com>
2024-07-06 12:02:32 +0800

    Check wiener mse

--- src/train.py
+++ src/train.py
@@ -68,11 +68,12 @@ def train(trainloader, validloader, model, criterion, optimizer, lrsch, logger, 
         loss_batch.backward()
         loss_logger += loss_batch.item()    # 显示全部loss
         optimizer.step()
-        lrsch.step()
-        sum_mse = criterion(torch.sum(outs.squeeze(),dim=1),gt_sum)
+        # lrsch.step()
+        sum_mse = criterion(torch.sum(outs.squeeze(1),dim=1),gt_sum)
         # print(sum_mse)  # debug
         mse_list.append(sum_mse.cpu().detach())
-
+    wiener_mse = criterion(torch.sum(noised.cuda()[:,2:3,:,:].squeeze(),dim=1),gt_sum).cpu().detach()
+    print('Compare with Wiener MSE:',wiener_mse)    # compare with winener mse
     loss_logger /= len(trainloader)
     print("Train loss:",loss_logger)
     log_metric('Train', mse_list, logger,loss_logger)
@@ -94,9 +95,10 @@ def test(testloader, model, criterion, optimizer, lrsch, logger, args):
         
         loss_batch = 100*criterion(outs,gt) + criterion(torch.sum(outs.squeeze(1),dim=1),gt_sum)
         loss_logger += loss_batch.item()    # 显示全部loss
-        sum_mse = criterion(torch.sum(outs.squeeze(),dim=1),gt_sum)
+        sum_mse = criterion(torch.sum(outs.squeeze(1),dim=1),gt_sum)
         mse_list.append(sum_mse.cpu().detach())
-        
+    wiener_mse = criterion(torch.sum(noised.cuda()[:,2:3,:,:].squeeze(),dim=1),gt_sum).cpu().detach()
+    print('Compare with Wiener MSE:',wiener_mse)    # compare with winener mse
     loss_logger /= len(testloader)
     print("Val loss:",loss_logger)
 
