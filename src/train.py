d774503f3d26c483ba1ffe06857f3f171f0f0cf1
auto-Dog <2969860272@qq.com>
2024-07-18 00:29:40 +0800

    Back to signal problem(14x256), add test image

--- src/train.py
+++ src/train.py
@@ -64,7 +64,7 @@ def train(trainloader, validloader, model, criterion, optimizer, lrsch, logger, 
         # print("opt tensor:",out)
         gt = gt.cuda()
         gt_sum = gt_sum.cuda()
-        loss_batch = 100*criterion(outs,gt) + criterion(torch.sum(outs.squeeze(1),dim=1),gt_sum)
+        loss_batch = 100*criterion(outs,gt) #+ criterion(torch.sum(outs.squeeze(1),dim=1),gt_sum)
         loss_batch.backward()
         loss_logger += loss_batch.item()    # 显示全部loss
         optimizer.step()
@@ -95,7 +95,7 @@ def test(testloader, model, criterion, optimizer, lrsch, logger, args):
         gt_sum = gt_sum.cuda()
         # print("label:",label)
         
-        loss_batch = 100*criterion(outs,gt) + criterion(torch.sum(outs.squeeze(1),dim=1),gt_sum)
+        loss_batch = 100*criterion(outs,gt) #+ criterion(torch.sum(outs.squeeze(1),dim=1),gt_sum)
         loss_logger += loss_batch.item()    # 显示全部loss
         sum_mse = criterion(torch.sum(outs.squeeze(1),dim=1),gt_sum)
         mse_list.append(sum_mse.cpu().detach())
